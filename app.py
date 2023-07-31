8132970458dd5f4034d36891a9dc2672685207dd
leeum23 <123345607+leeum23@users.noreply.github.com>
2023-07-31 14:41:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ import os
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.prompts.prompt import PromptTemplate
 
-openai.api_key=st.secrets["api"]
+openai.api_key=st.secrets["OPENAI_API_KEY"]
 
 
 def get_pdf_text(pdf_docs):
